84a3a3230d949b03f8d9085b1f8bba7af0a95522
Loganrud <lrudsenske25@sandomenico.org>
2024-01-24 15:10:49 -0800

    2.4.5 mostly working, needs to change iceSphere scale

--- Assets/Scripts/Other Controls/IceSphereController.cs
+++ Assets/Scripts/Other Controls/IceSphereController.cs
@@ -17,22 +17,48 @@ public class IceSphereController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        
+        if (GameManager.Instance.debugSpawnWaves)
+        {
+            reductionEachRepeat = 10f;
+        }
+        iceRB = GetComponent<Rigidbody>();
+        iceVFX = GetComponent<ParticleSystem>();
+        RandomizeSizeAndMass();
+        InvokeRepeating("Melt", 10.4f, startDelay);
     }
 
     // Update is called once per frame
     private void RandomizeSizeAndMass()
     {
+        transform.localScale *= 0.5f * Random.value + 0.5f;
         
     }
 
     private void Dissolution()
     {
+        float volume = 4.0f / 3.0f * Mathf.PI * Mathf.Pow(transform.localScale.x, 2);
+        int numOfIceSphereInScene = FindObjectsOfType<IceSphereController>().Length;
+        if (volume < 0.8f && numOfIceSphereInScene > 1)
+        {
+            iceVFX.Stop();
+        }
 
+        if (volume < minimumVolume)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Melt()
     {
-
+        if ((4 / 3) * Mathf.PI * Mathf.Pow(transform.localScale.x, 3) > 0.5f)
+        {
+            transform.localScale *= reductionEachRepeat;
+        }
+        else
+        {
+            Dissolution();
+        }
+        
     }
 }
